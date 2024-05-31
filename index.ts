3f6e572c763fd9c6bd28a2df20fe4c55b41dbeb2
hangsiahong <honghangsia@gmail.com>
2024-05-31 22:22:08 +0700

    update

--- index.ts
+++ index.ts
@@ -1,74 +1,18 @@
-import { checkInfoVirtualMachine } from './vm/checkInfoVirtualMachine';
-import { createVirtualMachine } from './vm/createVirtualMachine'; // Adjust the path if needed
-import { removeVirtualMachine } from './vm/removeVirtualMachine';
-import { startVirtualMachine } from './vm/startVirtualMachine';
-import { stopVirtualMachine } from './vm/stopVirtualMachine';
-import type { VMOptions } from './types/VMOptionsType';
+import express from "express";
+import vmRoutes from "./routes/vmRoutes";
 
+import dotenv from 'dotenv/config'
 
-
-import express from 'express';
-import vmRoutes from './routes/vmRoutes';
-import bodyParser from 'body-parser';
-
+const config = dotenv // will return an object
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', vmRoutes);
+app.use("/api", vmRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = Number(process.env.PORT || 3000); // Ensure PORT is a number
+const HOST = process.env.HOST || 'localhost';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Example usage
-// const vmOptions: VMOptions = {
-//   name: "koompi",
-//   iso: "koompi",
-//   ram: "4096",
-//   disk: "15G",
-//   cpu: "2",
-//   network: "br10",
-//   bootOption: 'uefi',
-//   arch: 'x64'
-// };
-
-// createVirtualMachine(vmOptions)
-//   .then(() => {
-//     console.log("Virtual machine created successfully.");
-//   })
-//   .catch((error) => {
-//     console.error("Failed to create virtual machine:", error);
-//   });
-
-// removeVirtualMachine({ name: vmOptions.name })
-
-// checkInfoVirtualMachine({ name: vmOptions.name })
-
-// startVirtualMachine({ name: vmOptions.name })
-
-// stopVirtualMachine({ name: vmOptions.name })
-
-// listAllVirtualMachine()
-// .then(() => {
-//   console.log("Listing all the virtual machine available.");
-// })
-// .catch((error) => {
-//   console.error("Failed to list all virtual machine:", error);
-// });
-
-
